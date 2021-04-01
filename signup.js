b648977735dee6eaf1d43f302b6dd6026f5351d0
Pallav14730 <81040573+Pallav14730@users.noreply.github.com>
2021-04-01 18:01:00 +0530

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const bodyparser=require('body-parser');
+const request=require('request');
+
+const { response } = require('express');
+var app = express();
+app.use(bodyparser.urlencoded({extended:true})) 
+
+app.use(express.static("public"))
+app.get("/",function(request,response){
+    response.sendFile(__dirname+"/signup.html")
+})
+app.post("/",function(request,response){
+    var firstname=request.body.fname;
+    var lastname=request.body.lname;
+    var email=request.body.email;
+    console.log("name is: " + firstname +  + "lastame is: " + lastname +  " email is: " + email);
+})
+app.post("/",function(request,response){
+    response.sendFile(__dirname + "/success.html"); 
+
+})
+app.listen(3000,function(){ 
+    console.log("server is running in 3000 port");
+})  
+    
